c811a5429f87b72bd7ecc1c81cfeb3b29fd86247
amosbaby <931963081@qq.com>
2022-06-10 10:08:08 +0800

    feat: 🎸 create project files and default style

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders 欢迎语', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/欢迎你大帅比/i);
   expect(linkElement).toBeInTheDocument();
 });
